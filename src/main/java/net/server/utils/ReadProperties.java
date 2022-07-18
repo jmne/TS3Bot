1571289a9ee36bbb30eeed5f134937f890954511
Jan <14363947+jmne@users.noreply.github.com>
2022-07-18 02:06:00 +0200

    Insert correct file path

--- src/main/java/net/server/utils/ReadProperties.java
+++ src/main/java/net/server/utils/ReadProperties.java
@@ -1,5 +1,6 @@
 package net.server.utils;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,11 +12,11 @@ public class ReadProperties {
     public ReadProperties() {
         try {
             prop = new Properties();
-            FileInputStream app = new FileInputStream("app.properties");
+            FileInputStream app = new FileInputStream(System.getProperty("user.dir") + "/TS3Bot/app.properties");
             if (app.available() > 0) {
                 prop.load(app);
             } else {
-                FileInputStream appDefault = new FileInputStream("app.default.properties");
+                FileInputStream appDefault = new FileInputStream(System.getProperty("user.dir") + "/TS3Bot/app.default.properties");
                 if (appDefault.available() > 0) {
                     prop.load(appDefault);
                 } else {
